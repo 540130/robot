4d6a6c51d49d1a8018886d8aa74c1c72261fbff6
brian <brian.kimuya@student.moringaschool.com>
2023-08-08 09:16:14 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Bot from './components/Bot';
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  const [data, setData]=useState([])
+  useEffect(()=>{
+    fetch(`http://localhost:8000/bots`)
+    .then((res)=>res.json())
+    .then((bots)=>{
+      setData(bots)
+    })
+  }, [])
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={data && <Bot bots={data}/>}/>
+          
+        </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
